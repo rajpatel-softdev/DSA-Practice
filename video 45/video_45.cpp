04012867941828ada0361a842e57e256259c02b5
rajpatel-softdev <rajvpatel21@gnu.ac.in>
2023-02-16 22:05:12 +0530

    video 45 added

---
+++ video 45/video_45.cpp
@@ -0,0 +1,8 @@
+//Linked List --> Question Solving
+
+// Code Studio --> Reverse Linked List --> https://www.codingninjas.com/codestudio/problems/reverse-linked-list_920513?leftPanelTab=0
+// both ittarative and recursive methods
+// HW -->reverse a Doublly Linked List
+
+// Code Studio --> Middle of Linked List --> https://www.codingninjas.com/codestudio/problems/middle-of-linked-list_973250?source=youtube&campaign=Lovebabbarcodestudio_24thJan&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbarcodestudio_24thJan&leftPanelTab=1
+
