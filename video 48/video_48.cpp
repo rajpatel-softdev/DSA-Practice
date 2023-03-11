28e2f3a95cc21283dcacc0e9c0150764f248cae1
rajpatel-softdev <rajvpatel21@gnu.ac.in>
2023-03-11 20:22:13 +0530

    lacture 48 and files added

---
+++ video 48/video_48.cpp
@@ -0,0 +1,11 @@
+// Lacture 48 --> Remove Duplicates from a Sorted/UnSorted Linked List
+
+//Coding Ninjas --> Remove Duplicates from Sorted Linked List --> https://bit.ly/3rjMQC5 
+
+// Homework --> Remove Duplicates from UNSORTED LL --> https://bit.ly/3uaGRRU 
+// Try all three approches:
+//      1. use 2 loops -> O(n^2) and O(1)
+//      2. first sort LL and then remove duplicates --> O(n log(n)) and O(1)
+//      3. using <map> --> O(n)
+
+// Homewrok --> split a circular LL in 2 halfs
